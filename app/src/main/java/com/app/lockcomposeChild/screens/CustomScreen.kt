8bf80f06aa1699cac51880c5a2a83453a2edbee6
DESKTOP-JJ86S6U\BreezyWeezy <eddinetaki087@gmail.com>
2025-01-12 20:47:11 +0100

    new changes

--- app/src/main/java/com/app/lockcomposeChild/screens/CustomScreen.kt
+++ app/src/main/java/com/app/lockcomposeChild/screens/CustomScreen.kt
@@ -279,6 +279,7 @@ fun askPermission(context: Context) {
     val map = hashMapOf<String, Any>()
     map["answer"] = "No"
     map["type"] = "Custom"
+    map["id"] = System.currentTimeMillis()
     val firebaseDatabase = FirebaseDatabase.getInstance().reference
     firebaseDatabase
         .child("Permissions")
